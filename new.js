c87c7c5a88550be88670706b7b2f669a6be8cc92
ChenHzt <chenhzt@gmail.com>
2021-03-08 00:42:27 +0200

    commit

--- new.js
+++ new.js
@@ -29,7 +29,6 @@ class Card {
 
   setRevealed() {
     this.cardHTML.setAttribute('data-status', 'revealed');
-
     this.status = 'revealed';
   }
 
@@ -59,24 +58,75 @@ Array.prototype.shuffle = function () {
   return this.sort(() => Math.random() - 0.5);
 };
 
+class Timer {
+  constructor() {
+    this.timeDisplay = document.querySelector('#timer');
+    this.startTimer();
+  }
+
+  startTimer(){
+    this.secondsPassed = 0;
+    this.minutesDisplay = 0;
+    this.secondsDisplay = 0;
+
+    this.timer = setInterval(()=>{
+      this.secondsPassed++;
+      const pad = (val) => {
+        let valString = val + "";
+        if (valString.length < 2) {
+          return "0" + valString;
+        } else {
+          return valString;
+        }
+      }
+      this.minutesDisplay = pad(Math.floor(this.secondsPassed / 60));
+      this.secondsDisplay = pad(this.secondsPassed % 60);
+      document.querySelector('#timer').textContent = `${this.minutesDisplay}:${this.secondsDisplay}`;
+    }, 1000);
+  }
+
+  stopTimer(){
+    clearInterval(this.timer);
+  }
+
+  resetTimer(){
+    this.stopTimer();
+    this.startTimer();
+  }
+
+
+}
+
+
 class Board {
   constructor(_pairAmount) {
+    this.timer = new Timer();
+    this.newGame(_pairAmount);
+    this.gameActive = true;
+    document.querySelector('#newGame').addEventListener('click', (event) => this.newGame(_pairAmount));
+  }
+
+  newGame(_pairAmount){
     this.openCards = [];
     this.pairAmount = _pairAmount;
     this.wrongTries = 0;
+    this.gameActive = true;
+    this.pairsFound = 0; 
     this.pairs = new Array(this.pairAmount);
     for (let i = 0; i < this.pairAmount; i++) this.pairs[i] = new Pair(i);
     this.toDisable = false;
     this.cards = new Array(this.pairAmount * 2);
     for (let i = 0; i < this.cards.length; i++)
       this.cards[i] = new Card(this.pairs[Math.floor(i / 2)]);
-
+    document.querySelector('.gameBoard').innerHTML = '';
     this.shuffleCards();
     this.cards.forEach((card) => card.renderCard());
     this.cards.forEach((card) =>
       card.cardHTML.addEventListener('click', (event) => this.cardClicked(card))
     );
+    this.timer.resetTimer();
   }
+
 
   shuffleCards() {
     this.cards = this.cards.shuffle();
@@ -99,6 +149,9 @@ class Board {
   rightGuess() {
     this.openCards.forEach((card) => card.setRevealed());
     this.openCards = [];
+    this.pairsFound++;
+    if(this.pairsFound === this.pairAmount)
+      this.endGame();
   }
 
   wrongGuess() {
@@ -106,11 +159,16 @@ class Board {
 
     setTimeout(() => {
       this.openCards.forEach((card) => card.setClosed());
-
       this.openCards = [];
       this.wrongTries++;
+      document.querySelector('#wrongAttempts').innerHTML = this.wrongTries+'';
       this.toDisable = false;
     }, 1000);
+  }
+
+  endGame(){
+    this.timer.stopTimer();
+    this.gameActive = false;
   }
 }
 
